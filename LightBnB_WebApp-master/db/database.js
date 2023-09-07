a65a9ac100e179f298782b78c2b572edb7a8629f
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-09-07 14:56:07 -0400

    adding add user function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -76,11 +76,23 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  console.log("user passed to function:" , user)
+  return pool
+  .query(`INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *`, [user.name, user.email, user.password])
+  .then((result)=> {
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message)
+  })
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
 };
 
 /// Reservations
